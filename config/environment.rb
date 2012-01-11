85e5571ca99fdd1e0a8abce9cf901d9dbaf42d17
Roberto Esposito <boborbt@gmail.com>
2012-01-11 14:40:03 +0100

    Removed gmail pwd from environment.rb (and changed the current gmail pwd of course…)

--- config/environment.rb
+++ config/environment.rb
@@ -63,9 +63,9 @@ Rails::Initializer.run do |config|
     :domain => "gmail.com", 
     :authentication => :plain, 
     :user_name => "sondaggio.statuto@gmail.com", 
-    :password => "letiziapietrovittoria" 
+    :password => File.read('gmail_pwd.txt') 
   }
-  
+    
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
